2a7d89405b966a47796bdcb62125b4f5a8ae228e
DmitriBelski <51079926+DmitriBelski@users.noreply.github.com>
2020-06-16 18:10:24 +0300

    finish css layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
-{
+module.exports = {
   "parser": "babel-eslint",
   "rules": {
     "semi": "off",
     "arrow-parens": "off",
     "comma-dangle": "off",
-    "require-jsdoc": "off"
+    "require-jsdoc": "off",
+    "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
+    // "linebreak-style": 0
+    // "linebreak-style": ["error", "windows"]
     // "no-unused-vars": "warn"
     // "no-unused-vars": "error"
   },
